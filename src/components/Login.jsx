089e6003b7b609bcce6bb75e99804a8951212002
Anthony Elison <tonyelison37@gmail.com>
2023-11-28 14:35:44 -0700

    Add basic login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,21 @@
 const Login = () => {
-  return <>Login Form</>
+  return (
+    <>
+      <h1>Login</h1>
+      <form action="http://localhost:3000/api/login" method="POST">
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
