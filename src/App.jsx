2da31505029c5b355b32a4ae35b75baa3c817d9e
Anthony Elison <tonyelison37@gmail.com>
2023-11-28 13:52:09 -0700

    Add function to test connecting to shop-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+// import { useEffect } from 'react';
+
 function App() {
+
+  const testApi = () => {
+    fetch('http://localhost:3000/api/hello')
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
+      <button onClick={testApi}>Test the API!</button>
     </>
   )
 }
