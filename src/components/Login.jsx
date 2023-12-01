889c448b2ee37b1ca01305e4210aa935c869d61c
Anthony Elison <tonyelison37@gmail.com>
2023-12-01 12:39:26 -0700

    Add login form submit handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,30 @@
 const Login = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // debugger;
+    const creds = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        mode: "cors",
+        headers: new Headers({ "Content-Type": "application/json" }),
+        body: JSON.stringify(creds)
+      });
+      response.json().then((data) => console.log(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <form action="http://localhost:3000/api/login" method="POST">
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">Username:</label>
           <input type="text" id="username" name="username" />
@@ -12,7 +34,7 @@ const Login = () => {
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" name="password" />
         </div>
-        <button type="submit">Log In</button>
+        <input type="submit" value="Log In" />
       </form>
     </>
   );
