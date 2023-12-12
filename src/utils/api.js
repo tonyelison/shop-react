36bf1496e3f26fd82a24f1e9bdef065872734aa5
Anthony Elison <tonyelison37@gmail.com>
2023-12-11 18:30:35 -0700

    Fix auth testing endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import http from './http';
 
 const api = (() => ({
   hello: () => http.get('hello'),
-  helloAuth: () => http.get('hello-auth'),
+  helloAuth: () => http.get('hello/auth'),
   register: (creds) => http.post('users', creds),
   login: (creds) => http.post('session', creds),
   logout: (creds) => http.del('session', creds),
