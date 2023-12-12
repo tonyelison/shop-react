e3ff1b7b74279feb8dac530d114f3d7497c60aea
Anthony Elison <tonyelison37@gmail.com>
2023-12-11 20:39:40 -0700

    Add layout flexbox that fills entire vh

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
+import Header from '@/components/Header';
+import './Layout.scss';
 
 const Layout = () => {
   return (
-    <>
+    <div id="layout">
       <Header />
       <main>
         <Outlet />
       </main>
       <footer>Copyright © 2023 Some Company Inc. All rights reserved.</footer>
-    </>
+    </div>
   );
 };
 
