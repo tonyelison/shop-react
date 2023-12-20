b0633d69f016e441e5a9839f2746c251b86a5257
Anthony Elison <tonyelison37@gmail.com>
2023-12-20 15:21:10 -0700

    Send along jwt token to verify user

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -1,11 +1,15 @@
+import { useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from 'react-bootstrap/Spinner';
 import http from '@/utils/http';
 
 const Verify = () => {
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get('token');
+
   const verifyQuery = useQuery({
     queryKey: ['verify'],
-    queryFn: () => http.get('verify'),
+    queryFn: () => http.get(`verify?token=${token}`),
     retry: false,
   });
   return (
